8da91dbbc05a2744e91e7b04174b674b2bb80c46
Carl Garner <carl@thispageonthe.net>
2021-12-10 23:22:23 +0000

    A bit of a shuffle of Day3 variables in case they're re-usable

--- src/main/kotlin/days/dayThree/dayThree.kt
+++ src/main/kotlin/days/dayThree/dayThree.kt
@@ -3,12 +3,12 @@ package days.dayThree
 import readFileLinesToString
 
 private val readings = readFileLinesToString("inputs/d03-p01.txt")
+private val counts = hashMapOf(0 to 0, 1 to 0, 2 to 0, 3 to 0, 4 to 0, 5 to 0, 6 to 0, 7 to 0, 8 to 0, 9 to 0, 10 to 0, 11 to 0)
 
 fun partOne(): Int {
     var total = readings.count()
     var gamma = ""
     var epsilon = ""
-    var counts = hashMapOf(0 to 0, 1 to 0, 2 to 0, 3 to 0, 4 to 0, 5 to 0, 6 to 0, 7 to 0, 8 to 0, 9 to 0, 10 to 0, 11 to 0)
 
     for(line in readings) {
         for(ch in line.indices) {
@@ -16,7 +16,7 @@ fun partOne(): Int {
         }
     }
 
-    //println("Counts: $counts")
+    println("Counts: $counts")
 
     for((key, value) in counts) {
         var test = total - value
@@ -37,3 +37,25 @@ fun partOne(): Int {
     return gamma.toInt(2) * epsilon.toInt(2)
 }
 
+fun partTwo(): Int {
+    var oxygen = ""
+    var co2 = ""
+    var oxyList: MutableList<String> = ArrayList<String>()
+    var co2List: MutableList<String> = ArrayList<String>()
+    var (zeros, ones) = readings.partition { it.startsWith('0') }
+
+    if(zeros.count() > ones.count()) {
+        oxyList.addAll(ones)
+        co2List.addAll(zeros)
+    } else {
+        oxyList.addAll(zeros)
+        co2List.addAll(ones)
+    }
+
+    println("Zero count: ${zeros.count()}")
+    println("Ones count: ${ones.count()}")
+
+
+
+    return 0
+}
