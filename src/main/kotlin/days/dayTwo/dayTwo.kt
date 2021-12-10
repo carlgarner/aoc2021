0770fd848aa67449fb0cc4579b815485f52424a6
Carl Garner <carl@thispageonthe.net>
2021-12-10 08:13:20 +0000

    Day 2 Part 2

--- src/main/kotlin/days/dayTwo/dayTwo.kt
+++ src/main/kotlin/days/dayTwo/dayTwo.kt
@@ -20,7 +20,28 @@ fun partOne(): Int {
         }
     }
 
-    //println("H: ${horizontal} - D: ${depth}")
+    return horizontal * depth
+}
+
+fun partTwo(): Int {
+    var horizontal = 0
+    var depth = 0
+    var aim = 0
+
+    for(item in readings) {
+        var output = item.split(" ")
+        var actionValue = output[1].toInt()
+
+        when(output[0]) {
+            "forward"   -> {
+                horizontal += actionValue
+                depth += (actionValue * aim)
+            }
+            "down"      -> aim += actionValue
+            "up"        -> aim -= actionValue
+            else        -> println("problem: ${output[0]} - ${output[1]}")
+        }
+    }
 
     return horizontal * depth
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,5 +4,6 @@ fun main() {
             "Day 1, Part 1 (v2): ${days.dayOne.partOneV2()} \n" +
             "Day 1, Part 2 (v1): ${days.dayOne.partTwo()} \n" +
             "Day 1, Part 2 (v2): ${days.dayOne.partTwoV2()} \n" +
-            "Day 2, Part 1: ${days.dayTwo.partOne()}")
+            "Day 2, Part 1: ${days.dayTwo.partOne()}\n" +
+            "Day 2, Part 2: ${days.dayTwo.partTwo()}")
 }
