2d5325c9da79e90cb0ce18dbc3f5d92f2f2c5fad
Carl Garner <carl@thispageonthe.net>
2021-12-10 10:56:56 +0000

    Day 3 part 1 and inputs

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,5 +5,6 @@ fun main() {
             "Day 1, Part 2 (v1): ${days.dayOne.partTwo()} \n" +
             "Day 1, Part 2 (v2): ${days.dayOne.partTwoV2()} \n" +
             "Day 2, Part 1: ${days.dayTwo.partOne()}\n" +
-            "Day 2, Part 2: ${days.dayTwo.partTwo()}")
+            "Day 2, Part 2: ${days.dayTwo.partTwo()}\n" +
+            "Day 3, Part 1: ${days.dayThree.partOne()}")
 }
